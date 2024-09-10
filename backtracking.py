4c2902d6db389d4a192d877da391f5501f68700b
Adrien S <skrzypczak.adrien@gmail.com>
2024-09-10 09:24:13 +0200

    Update backtracking.py

--- backtracking.py
+++ backtracking.py
@@ -3,16 +3,16 @@
 def combine(self, n, k):
     res = []
 
-    def dfs(nums, k, index, path):
+    def backtrack(nums, k, index, path):
         #if k < 0:  #backtracking
             #return 
         if k == 0:
             res.append(path)
             return # backtracking 
         for i in range(index, len(nums)):
-            dfs(nums, k - 1, i + 1, path + [nums[i]])
+            backtrack(nums, k - 1, i + 1, path + [nums[i]])
 
-    dfs(range(1, n + 1), k, 0, [])
+    backtrack(range(1, n + 1), k, 0, [])
     return res
 
 # ----------------------------------------------------------------------------
@@ -21,14 +21,14 @@ def combine(self, n, k):
 def permute(self, nums: List[int]) -> List[List[int]]:
     res = []
 
-    def dfs(nums, path):
+    def backtrack(nums, path):
         if not nums:
             res.append(path)
             #return # backtracking
         for i in range(len(nums)):
-            dfs(nums[:i]+nums[i+1:], path + [nums[i]])
+            backtrack(nums[:i]+nums[i+1:], path + [nums[i]])
 
-    dfs(nums, [])
+    backtrack(nums, [])
     return res
 
 # ----------------------------------------------------------------------------
@@ -38,7 +38,7 @@ def permuteUnique(self, nums):
     res, visited = [], [False] * len(nums)
     nums.sort()
 
-    def dfs(nums, visited, path):
+    def backtrack(nums, visited, path):
         if len(nums) == len(path):
             res.append(path)
             return 
@@ -47,10 +47,10 @@ def permuteUnique(self, nums):
                 if i > 0 and not visited[i - 1] and nums[i] == nums[i - 1]:  # here should pay attention
                     continue
                 visited[i] = True
-                dfs(nums, visited, path + [nums[i]])
+                backtrack(nums, visited, path + [nums[i]])
                 visited[i] = False
 
-    dfs(nums, visited, [])
+    backtrack(nums, visited, [])
     return res
 
 # ----------------------------------------------------------------------------
@@ -59,12 +59,12 @@ def permuteUnique(self, nums):
 def subsets1(self, nums):
     res = []
 
-    def dfs(index, path):
+    def backtrack(index, path):
         res.append(path)
         for i in range(index, len(nums)):
-            dfs(i + 1, path + [nums[i]])
+            backtrack(i + 1, path + [nums[i]])
 
-    dfs(0, [])
+    backtrack(0, [])
     return res
 
 # ----------------------------------------------------------------------------
@@ -74,14 +74,14 @@ def subsetsWithDup(self, nums):
     res = []
     nums.sort()
 
-    def dfs(nums, index, path):
+    def backtrack(nums, index, path):
         res.append(path)
         for i in range(index, len(nums)):
             if i > index and nums[i] == nums[i - 1]:
                 continue
-            dfs(nums, i + 1, path + [nums[i]])
+            backtrack(nums, i + 1, path + [nums[i]])
 
-    dfs(nums, 0, [])
+    backtrack(nums, 0, [])
     return res
 
 # ----------------------------------------------------------------------------
@@ -91,16 +91,16 @@ def combinationSum(self, candidates, target):
     res = []
     candidates.sort()
 
-    def dfs(nums, target, index, path):
+    def backtrack(nums, target, index, path):
         if target < 0:
             return  # backtracking
         if target == 0:
             res.append(path)
             return 
         for i in range(index, len(nums)):
-            dfs(nums, target - nums[i], i, path + [nums[i]])
+            backtrack(nums, target - nums[i], i, path + [nums[i]])
 
-    dfs(candidates, target, 0, [])
+    backtrack(candidates, target, 0, [])
     return res
 
 # ----------------------------------------------------------------------------
@@ -110,7 +110,7 @@ def combinationSum2(self, candidates, target):
     res = []
     candidates.sort()
 
-    def dfs(candidates, target, index, path):
+    def backtrack(candidates, target, index, path):
         if target < 0:
             return  # backtracking
         if target == 0:
@@ -119,7 +119,7 @@ def combinationSum2(self, candidates, target):
         for i in range(index, len(candidates)):
             if i > index and candidates[i] == candidates[i - 1]:
                 continue
-            dfs(candidates, target - candidates[i], i + 1, path + [candidates[i]])
+            backtrack(candidates, target - candidates[i], i + 1, path + [candidates[i]])
 
-    dfs(candidates, target, 0, [])
+    backtrack(candidates, target, 0, [])
     return res
