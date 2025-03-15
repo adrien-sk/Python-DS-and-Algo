143d5f34d0c3ec07273d1731345ae9af6744416b
adrien-sk <skrzypczak.adrien@gmail.com>
2025-03-15 18:12:41 +0100

    update merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,5 +1,7 @@
 # Recursive solution
 
+# On place : updating the array
+
 def mergeSort(arr):
 	if len(arr) > 1:
 		mid = len(arr) // 2
@@ -31,3 +33,34 @@ def mergeSort(arr):
 			arr[k] = right[j]
 			j += 1
 			k += 1
+
+
+# With return statement
+
+def sortArray(self, nums: List[int]) -> List[int]:
+	if len(nums) < 2:
+		return nums
+	
+	mid = len(nums) // 2
+	left = self.sortArray(nums[:mid])
+	right = self.sortArray(nums[mid:])
+	p1, p2 = 0, 0
+	res = []
+	
+	while p1 < len(left) and p2 < len(right):
+		if left[p1] <= right[p2]:
+			res.append(left[p1])
+			p1 +=1
+		else:
+			res.append(right[p2])
+			p2 +=1
+	
+	while p1 < len(left):
+		res.append(left[p1])
+		p1 += 1
+	
+	while p2 < len(right):
+		res.append(right[p2])
+		p2 += 1
+
+	return res
